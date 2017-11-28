7e422728919d3634c55766f6190d6691aa6896c6
TJ Stubbs <stubbsrule@gmail.com>
2017-11-28 18:46:16 -0500

    finished game logic working

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
+//if(keydown.characterCode == 32) {console.log(characterCode)}
 //JS logic
 
-//Grab the player element
+//Grab the player element and create variables
 const playerOne = document.getElementById('player1');
 const playerTwo = document.getElementById('player2');
+//event
 
-document.addEventListener('keydown', movePlayer);
-//push the player element to the right by one increment each time a button is pressed
+//event listener waiting for keydown to move players
+document.addEventListener('keydown', movePlayerOne);
+document.addEventListener('keydown', movePlayerTwo);
+//starting position for players
 let playerOneVal = 0;
-function movePlayer(){
-	playerOneVal = playerOneVal + 5;
+let playerTwoVal = 0;
+
+
+//player 1 movement function
+
+
+function movePlayerOne(){
+	//increments players position by 5px every key press
+	if(event.keyCode == 32){
+	playerOneVal = playerOneVal + 100;
+	//push the player element to the right by one increment each time a button is pressed
 	playerOne.style.left = playerOneVal + 'px';
 }
-//
+isOneFinish();
+}
+
+//Player 2 movement function
+
+//if(event.keycode == '13'){
+
+function movePlayerTwo(){
+	if(event.keyCode == 13){
+	playerTwoVal = playerTwoVal + 100;
+	//push the player element to the right by one increment each time a button is pressed
+	playerTwo.style.left = playerTwoVal + 'px';
+}
+isTwoFinish();
+}
+
+function isOneFinish(){
+	if (playerOneVal >= 850) {
+		location.href = 'finish.html';
+	}
+} 
+
+function isTwoFinish(){
+	if (playerTwoVal >= 850) {
+		location.href = 'finish.html';
+	}
+} 
