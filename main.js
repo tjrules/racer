f0372e8f01292b5f492762a4adea01b3672a8740
TJ Stubbs <stubbsrule@gmail.com>
2017-11-28 20:20:27 -0500

    Perfect game

--- main.js
+++ main.js
@@ -18,8 +18,10 @@ let playerTwoVal = 0;
 
 
 function movePlayerOne(){
-	//increments players position by 5px every key press
+	
+	//ensures specific key is pressed for function to run
 	if(event.keyCode == 32){
+	//increments players position every key press
 	playerOneVal = playerOneVal + 100;
 	//push the player element to the right by one increment each time a button is pressed
 	playerOne.style.left = playerOneVal + 'px';
@@ -32,22 +34,24 @@ isOneFinish();
 //if(event.keycode == '13'){
 
 function movePlayerTwo(){
+	//ensures specific key is pressed for function to run
 	if(event.keyCode == 13){
 	playerTwoVal = playerTwoVal + 100;
 	//push the player element to the right by one increment each time a button is pressed
 	playerTwo.style.left = playerTwoVal + 'px';
 }
+//declares function that checks for win
 isTwoFinish();
 }
 
 function isOneFinish(){
-	if (playerOneVal >= 850) {
+	if (playerOneVal >= 950) {
 		location.href = 'finish.html';
 	}
 } 
 
 function isTwoFinish(){
-	if (playerTwoVal >= 850) {
-		location.href = 'finish.html';
+	if (playerTwoVal >= 950) {
+		location.href = 'finish-2.html';
 	}
 } 
